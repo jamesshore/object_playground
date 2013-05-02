59912d67d81c0dd736d3d57897660377a39053b1
James Shore <jshore@jamesshore.com>
2013-05-02 11:47:19 -0700

    Documented ./convert_code.pl, made it more convenient

--- convert_code.pl
+++ convert_code.pl
@@ -1,8 +1,19 @@
 #!/usr/bin/perl
 
-my @lines = <STDIN>;
+# Converts input into JavaScript. Intended to make it easier to generate code samples.
+# To use:
+# 1. Run this program from the terminal (`./convert_code.pl`).
+# 2. Paste in code to convert.
+# 3. Press ^D (control-D).
+# 4. Copy output where it needs to go.
 
-foreach (@lines) {
-	$_ =~ s/\n/\\n/g;
-	print "\"$_\" +\n";
+my @lines = <STDIN>;
+foreach $line (@lines) {
+	$line =~ s/\t/  /g;
+	$line =~ s/\n/\\n/g;
+	$line =~ s/\'/\\'/g;
 };
+
+$output = join "'+ \n'", @lines;
+print "\n\nOUTPUT:\n";
+print "'$output';\n";
