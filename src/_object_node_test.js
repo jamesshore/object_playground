c37018667754db43a47a44ddb8e7c98eb691935f
James Shore <jshore@jamesshore.com>
2013-07-17 12:21:42 -0700

    Fixed copyright dates

--- src/_object_node_test.js
+++ src/_object_node_test.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+// Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
 
 (function() {
 	"use strict";
