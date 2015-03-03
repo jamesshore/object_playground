33f05ce2d08f5fce224ae10ad66bd0da2276c65d
James Shore <jshore@jamesshore.com>
2015-03-03 12:06:51 -0800

    Installed Karma Expect.js and Mocha plugins

--- build/config/karma.conf.js
+++ build/config/karma.conf.js
@@ -1,63 +1,79 @@
 // Karma configuration
-// Generated on Wed Oct 10 2012 14:10:32 GMT-0700 (PDT)
+// Generated on Tue Sep 24 2013 14:33:42 GMT-0700 (PDT)
+(function() {
+	"use strict";
+
+	module.exports = function(config) {
+	  config.set({
+
+	    // base path, that will be used to resolve files and exclude
+	    basePath: '../..',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '../..';
+	    // frameworks to use
+	    frameworks: ['mocha', 'expect'],
 
 
-// list of files / patterns to load in the browser
-files = [
-  MOCHA,
-  MOCHA_ADAPTER,
-  'node_modules/expect.js/expect.js',
-	'src/viz/viz.js',
-  'src/*.js'
-];
+	    // list of files / patterns to load in the browser
+	    files: [
+	      'src/client/**/*.js'
+	    ],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+	    // list of files to exclude
+	    exclude: [
+
+	    ],
 
 
-// test results reporter to use
-// possible values: dots || progress
-reporter = 'progress';
+	    // preprocess matching files before serving them to the browser
+	    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+	    preprocessors: {
+		    'src/client/**/*.js': ['commonjs'],
+	    },
 
 
-// web server port
-port = 8080;
+	    // test results reporter to use
+	    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+	    reporters: ['progress'],
 
 
-// cli runner port
-runnerPort = 9100;
+	    // web server port
+	    port: 9876,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+	    // enable / disable colors in the output (reporters and logs)
+	    colors: true,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+	    // level of logging
+	    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+	    logLevel: config.LOG_INFO,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+	    // enable / disable watching file and executing tests whenever any file changes
+	    autoWatch: false,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari
-// - PhantomJS
-browsers = [];
+	    // Start these browsers, currently available:
+	    // - Chrome
+	    // - ChromeCanary
+	    // - Firefox
+	    // - Opera
+	    // - Safari (only Mac)
+	    // - PhantomJS
+	    // - IE (only Windows)
+	    browsers: [],
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+	    // If browser does not capture in given timeout [ms], kill it
+	    captureTimeout: 60000,
+
+
+	    // Continuous Integration mode
+	    // if true, it capture browsers, run tests and exit
+	    singleRun: false
+	  });
+	};
+
+}());
---
+++ node_modules/karma-expect/index.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+var pattern = function(file) {
+  return {pattern: file, included: true, served: true, watched: false};
+};
+
+var framework = function(files) {
+  files.unshift(pattern(path.resolve(require.resolve('expect.js'))));
+};
+
+framework.$inject = ['config.files'];
+module.exports = {'framework:expect': ['factory', framework]};
